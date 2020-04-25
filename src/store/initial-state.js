3d13518576237ef4916b04a838105e3ad8fc9443
Chris Long <chris_mark_long121@hotmail.com>
2020-04-25 09:15:10 +0100

    Toomy told me

---
+++ src/store/initial-state.js
@@ -0,0 +1,37 @@
+import GAME_STATES from './constants.js';
+import params from '../game-functions/params.js';
+
+const initialState = {
+    debug : true,
+    game : {
+        gameState : GAME_STATES.NO_GAME,
+        round : 0,
+        requirement : 0,
+    },
+    player : {
+        hand : [],
+        cardSelected : null,
+        table : {
+
+        },
+        isDown : false,
+        canGoDown : false,
+        isGoingDown : false
+    },
+    AI : {
+        players : [{
+            hand : [], //cards in hand
+            isDown : false, //has cards on table
+            table : [{
+                type : null, //will get set to "RUN" or "SET"
+                cards : [] //array of cards in set or run
+            }]
+        }],
+        AICount : params.numberOfPlayers - 1,
+        AIInPlay : null,
+    },
+    deck : [],
+    discard : []
+}
+
+export default initialState;
