e90d8226453c284795d8ddaf208038c33360a80a
Chris Long <chris_mark_long121@hotmail.com>
2020-04-27 08:50:26 +0100

    AI updates

--- src/game-functions/decisiontesting.js
+++ src/game-functions/decisiontesting.js
@@ -1,9 +1,12 @@
-// hand for testing sets.
+
 const hand = [
+    //"2D", "3D", "4D", "6D", "7D", "9H",
+    //"2C", "12C", "11S", "3H", "9S", "4S"
+    "12D", "4H", "2D",
     "2C", "2H", "13H",
-    "3D", "4S", "5C",
-    "5D", "5S", "12H",
-    "12D", "12D", "2H"   
+    "3C", "4C", "5C",
+    "8D", "9D", "10D"
+      
 ];
 
 const requirement = {
@@ -21,6 +24,10 @@ const scoreHand = (hand, requirement) => {
         return {value : parseInt(value), suit : suit, index : index}
     });
 
+    if (requirement.R != 0){
+        //we have runs!
+        let runsResult = getRuns(handArray);
+    }
     if (requirement.R === 0){
         let setsResult = getSets(handArray);
         let score = 0;
@@ -68,20 +75,101 @@ const scoreHand = (hand, requirement) => {
 
 const sortHand = (sortArray, param) => {
     //Sort for sets.
-    if (param = "SETS") {
-        sortArray.sort((a, b) => {
+    if (param === "SETS") {
+        return sortArray.sort((a, b) => {
             if (a.value < b.value) {
                 return -1;
             }else {
                 return 1;
             }
         });
-        return sortArray;
     }
-    if (param = "RUNS") { 
-        //run sorting needs to be implemented still.
-        return sortArray;
+    if (param === "RUNS") { 
+        return sortArray.sort((a, b) => {
+            if (a.suit < b.suit) {return - 1}
+            if (b.suit < a.suit) {return 1}
+            if (a.value < b.value) {return - 1}
+            if (b.value < a.value) {return 1}
+        });
     }
+}
+
+getRuns = (handArray) => {
+    let sortedHand = sortHand(handArray, "RUNS");
+    let activeSuit = sortedHand[0].suit;
+    let runLength = 1;
+    let complete = false;
+    let runKeys = [sortedHand[0].index];
+    let runValues = [sortedHand[0].value];
+    let potentialKeys = [];
+    let runs = [];
+    console.log(sortedHand);
+    for (let i = 1; i < sortedHand.length; i++){ 
+        if (sortedHand[i].suit === sortedHand[i-1].suit && sortedHand[i].value === sortedHand[i-1].value + 1){
+            //we have 2 consecutive cards.
+            runLength ++;
+            runKeys.push(sortedHand[i].index);
+            runValues.push(sortedHand[i].value);
+
+        }else{
+            //if we have a run, store it. in any case, reset.
+            if (runLength >= 2) {
+                runLength >= 4 ? complete = true : complete = false;
+                //store the run
+                runs.push({
+                    type : "run",
+                    complete : complete,
+                    values : runValues,
+                    suit : activeSuit,
+                    keys : runKeys,
+                    score : runLength * 20
+                });
+            }
+            runKeys = [sortedHand[i].index];
+            runLength = 1;
+            activeSuit = sortedHand[i].suit;
+            complete = false;
+            runValues = [sortedHand[i].value]
+        }
+        if (i === sortedHand.length -1 && runLength >= 2){
+            runLength >= 4 ? complete = true : complete = false;
+            //store the run
+            runs.push({
+                type : "run",
+                complete : complete,
+                values : runValues,
+                suit : activeSuit,
+                keys : runKeys,
+                score : runLength * 20
+            });
+        }
+    }
+    //add potential tag-ons to runs
+    runs.forEach(run => {
+        potentialKeys = [];
+        console.log(run);
+        sortedHand.forEach(card => {
+            if(card.suit === run.suit && 
+                !(run.keys.includes(card.index)) && 
+                (Math.abs(card.value - run.values[0]) < 4 || Math.abs(card.value - run.values[run.values.length -1]) < 4 )){
+                    
+                    potentialKeys.push(card.index);
+            }
+        });
+        run.potentialKeys = potentialKeys;
+        run.score = run.score + (potentialKeys.length * 5);
+    });
+
+    runs.sort((a, b) => {
+        if (a.score > b.score) {return -1}
+        if (a.score < b.score) {return 1}
+        if (a.values.reduce((a,b) => a+b) > b.values.reduce((a,b) => a+b)){return -1}
+        if (a.values.reduce((a,b) => a+b) < b.values.reduce((a,b) => a+b)){return 1}
+    });
+
+    
+
+    console.log(runs);
 }
 
 getSets = (handArray) => {
